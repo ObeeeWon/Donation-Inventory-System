c86ceaccf2b757b06b451b4f0033029d1b7f2de4
T0XICTAC0 <39878135+T0XICTAC0@users.noreply.github.com>
2025-03-06 16:46:57 -0400

    Branch Created
    Created my own branch to help with overwriting things.
    
    I have created the blade for the Insert page, as well as the Model and Controller. I had to make a new Migration for Items.
    
    The Controller can be used for the Home and Edit pages as well.
    
    Low Stock Reminder function can be done once the Home page is finished.

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('items.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'ItemName' => 'required|max:100|unique:items,ItemName',
+            'Barcode' => 'required|unique:items,Barcode',
+            'Quantity' => 'required',
+            'LowStockAlert' => 'required',
+            'Location' => 'required',
+        ];
+        $validator = $this->validate($request, $rules);
+
+        $item = new \App\Models\Item;
+        $item->ItemName = $request->ItemName;
+        $item->Barcode = $request->Barcode;
+        $item->Quantity = $request->Quantity;
+        $item->LowStockAlert = $request->LowStockAlert;
+        $item->Location = $request->Location;
+        $item->save();
+
+        Session::flash('success', 'New Item Added');
+
+        return redirect()->route('PLACEHOLDER');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Item $item)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Item $item)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Item $item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Item $item)
+    {
+        //
+    }
+}
