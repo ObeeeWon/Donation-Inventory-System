240f0c8d45163daf2191c639a9a9a7d8c3e7eb26
ObeeeWon <142458345+ObeeeWon@users.noreply.github.com>
2025-03-10 16:22:22 -0300

    Your commit message here

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// display the Item page created
+Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
+
+// process Item page
+Route::post('/items', [ItemController::class, 'store'])->name('items.store');
