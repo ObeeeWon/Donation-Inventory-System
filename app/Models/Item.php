f850248f774bf13a0a1cec5edfd88469a8f469a3
ObeeeWon <142458345+ObeeeWon@users.noreply.github.com>
2025-03-10 16:10:46 -0300

    Save local changes before pulling

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'items'; 
+
+    protected $primaryKey = 'ItemID'; 
+
+    public $timestamps = true; // Laravel will set the time stamp
+
+    protected $fillable = [
+        'ItemName',
+        'Barcode',
+        'Quantity',
+        'LowStockAlert',
+        'Location'
+    ];
 }
